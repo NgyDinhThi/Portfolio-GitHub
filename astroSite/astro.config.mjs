30ba3d10abe99f25464fba2677dc5e6e80dab836
NgDinhThi <dinhthit25@gmail.com>
2026-03-13 16:30:04 +0700

    deploy lại

--- astroSite/astro.config.mjs
+++ astroSite/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://ngydinhthi.github.io/Portfolio-GitHub/',
+  site: 'https://ngydinhthi.github.io',
   base: '/Portfolio-GitHub',
   vite: {
     plugins: [tailwindcss()],
